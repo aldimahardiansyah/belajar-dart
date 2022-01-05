3543e60094cd559e8ab280fb9a00bc0cd26d22a7
aldimahardiansyah <aldimdiansyah@gmail.com>
2022-01-05 11:35:14 +0700

    belajar foreach

---
+++ foreach.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+main() {
+  var languages = ['C', 'C++', 'Java', 'Dart', 'JavaScript'];
+
+  for (var language in languages) {
+    print(language);
+  }
+
+  print("Total bahasa: ${languages.length}");
+}
